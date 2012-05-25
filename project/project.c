571ca1696fb821e9902f4f81e2370327912404cf
Jukka Nousiainen <nousiaisenjukka@gmail.com>
2012-05-26 00:33:51 +0300

    done with select:

--- project/project.c
+++ project/project.c
@@ -6,25 +6,61 @@
 #define NUM_OF_DESCRIPTORS 20
 #define VALUES_PER_DESCRIPTOR 5
 
+/* calculate delta for two timespecs */
+long time_delta(struct timespec start, struct timespec end)
+{
+
+    long diff = end.tv_nsec - start.tv_nsec;
+    if(diff < 0)
+        diff += 1000000000;
+    return diff;
+}
+
 int main(int argc, char *argv[]) {
     int sensorDescriptors[NUM_OF_DESCRIPTORS];
     Tmeas measurement;
-
-    int fd_kb, fd_line, fd_display, i, n;
+    struct timespec current_time;
+    int i, j, descriptors_returned, closed = 0, readings = 0;
+    long time_diff = 0, total = 0, minimum = 0, maximum = 0;
     fd_set fdset;
 
     StartSimulator(sensorDescriptors, VALUES_PER_DESCRIPTOR);
 
-    while(1) {
-        FD_ZERO(&fdset);
+    FD_ZERO(&fdset);
+
+    while(closed <= NUM_OF_DESCRIPTORS) {
         for(i = 0; i < NUM_OF_DESCRIPTORS; i++) {
             FD_SET(sensorDescriptors[i], &fdset);
         }
+        descriptors_returned = select(sensorDescriptors[NUM_OF_DESCRIPTORS - 1] + 1, &fdset, NULL, NULL, NULL);
 
-        n = select(sensorDescriptors[NUM_OF_DESCRIPTORS - 1] + 1, &fdset, NULL, NULL, NULL);
+        if(descriptors_returned) {
+            for(j = 0; j < NUM_OF_DESCRIPTORS; j++) {
+                if(FD_ISSET(sensorDescriptors[j], &fdset)) {
+                    if(read(sensorDescriptors[j], &measurement, sizeof(Tmeas)) <= 0) {
+                        close(sensorDescriptors[j]);
+                        printf("   * descriptor %d closed\n", j);
+                        closed++;
+                    }
+                    else {
+                        clock_gettime(CLOCK_REALTIME, &current_time);
+                        time_diff = time_delta(measurement.moment, current_time);
+                        printf("value: %d ", measurement.value);
+                        printf("timediff: %d ns\n", time_diff);
+                        readings++;
 
-        if(FD_ISSET(sensorDescriptors[0], &fdset)) {
-            read(sensorDescriptors[0], &measurement, sizeof(Tmeas));
+                        /* increase total diff count for counting average */
+                        total += time_diff;
+
+                        /* update maximum and minimum, if necessary */
+                        if(minimum == 0 || time_diff < minimum)
+                            minimum = time_diff;
+                        if(maximum == 0 || time_diff > maximum)
+                            maximum = time_diff;
+                    }
+                }
+            }
         }
     }
+    printf("all measurements done, number of readings: %d\n  average: %d\n  minimum: %d\n  maximum: %d\n", readings, total / (VALUES_PER_DESCRIPTOR*NUM_OF_DESCRIPTORS), minimum, maximum);
 }
